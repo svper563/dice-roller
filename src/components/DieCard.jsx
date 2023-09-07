f2a053d76a2fe179ecf73fd2717a94828a67aa0f
svper563 <=>
2023-09-07 13:46:38 -0400

    Added increment/decrement functions

--- src/components/DieCard.jsx
+++ src/components/DieCard.jsx
@@ -6,12 +6,23 @@ export default function DieCard(props) {
 
     const [count, setCount] = useState(0);
 
+    function incrementCount() {
+        setCount(count + 1);
+    };
+
+    function decrementCount() {
+        if (count > 0) {
+            setCount(count - 1);
+        };
+    };
+
     return (
         <div className="die-card">
             <h2>{label}</h2>
             <img src={iconSrc}></img>
-            <button>+</button>
-            <button>-</button>
+            <p>{count}</p>
+            <button onClick={incrementCount}>+</button>
+            <button onClick={decrementCount}>-</button>
         </div>
     );
 }
