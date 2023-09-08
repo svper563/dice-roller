25a8a03e572bf769f3daf4b99b15d489897ae02b
svper563 <=>
2023-09-08 10:21:30 -0400

    Simplified DieCard component

--- src/components/DieCard.jsx
+++ src/components/DieCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export default function DieCard(props) {
 
-    const {label, iconSrc, sides} = props;
+    const {iconSrc, sides} = props;
 
     const [count, setCount] = useState(0);
 
@@ -18,11 +18,7 @@ export default function DieCard(props) {
 
     return (
         <div className="die-card">
-            <h2>{label}</h2>
             <img src={iconSrc} className="inverted"></img>
-            <p>{count}</p>
-            <button onClick={incrementCount}>+</button>
-            <button onClick={decrementCount}>-</button>
         </div>
     );
 }
