7a9b650f7208a79d794a46799b92cd2372c55683
svper563 <btteachey@gmail.com>
2023-09-08 10:55:58 -0400

    Initial dice rolling functionality

--- src/components/DieCard.jsx
+++ src/components/DieCard.jsx
@@ -2,23 +2,24 @@ import React, { useState } from "react";
 
 export default function DieCard(props) {
 
-    const {iconSrc, sides} = props;
+    const {iconSrc, sides, setDice} = props;
 
     const [count, setCount] = useState(0);
 
-    function incrementCount() {
-        setCount(prevCount => prevCount + 1);
-    };
+    function handleClick() {
+        console.log(`${sides} Button clicked.`);
+        const roll = rollDie();
+        const obj = {die: sides, roll: roll};
+        setDice(prevDice => [...prevDice, obj]);
+    }
 
-    function decrementCount() {
-        if (count > 0) {
-            setCount(prevCount => prevCount - 1);
-        };
-    };
+    function rollDie() {
+        return Math.floor(Math.random() * sides) + 1;
+    }
 
     return (
         <div className="die-card">
-            <button className="button-clear" onClick={() => console.log(`${sides} Button clicked.`)}>
+            <button className="button-clear" onClick={handleClick}>
                 <img src={iconSrc} className="inverted"></img>
             </button>
         </div>
--- src/components/DieCards.jsx
+++ src/components/DieCards.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import DieCard from "./DieCard";
 
 export default function DieCards() {
+
+    const [dice, setDice] = useState([]);
+
     return (
-        <div className="die-cards">
-            <DieCard sides={4} iconSrc="./icons/dice-d4.png"/>
-            <DieCard sides={6} iconSrc="./icons/dice-d6.png"/>
-            <DieCard sides={8} iconSrc="./icons/dice-d8.png"/>
-            <DieCard sides={12} iconSrc="./icons/dice-d12.png"/>
-            <DieCard sides={20} iconSrc="./icons/dice-d20.png"/>
+        <div>
+            <div className="die-cards">
+                <DieCard sides={4} iconSrc="./icons/dice-d4.png" setDice={setDice}/>
+                <DieCard sides={6} iconSrc="./icons/dice-d6.png" setDice={setDice}/>
+                <DieCard sides={8} iconSrc="./icons/dice-d8.png" setDice={setDice}/>
+                <DieCard sides={12} iconSrc="./icons/dice-d12.png" setDice={setDice}/>
+                <DieCard sides={20} iconSrc="./icons/dice-d20.png" setDice={setDice}/>
+            </div>
+            {dice?.map((die, index) => {
+                    return (
+                        <ul key={index} className="die">
+                            <li>{die.roll}</li>
+                        </ul>
+                    )
+                })}
         </div>
     )
 }
